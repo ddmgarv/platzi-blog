312bd03b1242d5d75e8d808ac09f92f8c6ba2ff7
Gustavo <garv1998131@gmail.com>
2019-09-16 20:45:37 -0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Menu from "./menu";
+
+
+const App = () => (
+  <BrowserRouter>
+    <Menu />
+  </BrowserRouter>
+);
+
+export default App;
---
+++ src/components/menu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Menu = () => (
+    <nav>
+        <a href="">
+            usuarios
+        </a>
+        <a href="">
+            Tareas
+        </a>
+    </nav>
+);
+
+export default Menu;
